baa351433a4308f6c1c110195b74b63102646207
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-12-01 17:48:30 +0900

    Refactor

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,27 +3,18 @@ export default function Experience({ data }) {
         <div>
             <h3>Right-side data as plain text</h3>
             {data.types.map((type) => {
-                const tempKeys = Object.entries(data.expInfo[type]);
+                const pairsExp = Object.entries(data.expInfo[type]);
                 return (
                     <div key={type}>
-                        {tempKeys.map((keysGeneral) => {
-                            const keys = Object.keys(keysGeneral[1]);
-                            return (
-                                <div key={keysGeneral[0]}>
-                                    {keys.map((key) => {
-                                        return (
-                                            <span key={key}>
-                                                {key}:{" "}
-                                                {
-                                                    data.expInfo[type][
-                                                        keysGeneral[0]
-                                                    ][key]
-                                                }
-                                            </span>
-                                        );
-                                    })}
-                                </div>
-                            );
+                        {pairsExp.map((pair) => {
+                            const labels = Object.keys(pair[1]);
+                            return labels.map((label, index) => {
+                                return (
+                                    <span key={index}>
+                                        {label}: {pair[1][label]}
+                                    </span>
+                                );
+                            });
                         })}
                     </div>
                 );
